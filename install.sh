f41f22197a9669e56810dd4d3cdf76fe9cd1107d
wangml71 <wangml71@163.com>
2024-01-21 14:05:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cp -rf ./Hector_ROS_Simulation/* ../catkin_ws/src/
+<<<<<<< Updated upstream
 cd ../catkin_ws/
 rm -rf build devel
+=======
+cd ../catkin_ws
+>>>>>>> Stashed changes
 catkin_make
